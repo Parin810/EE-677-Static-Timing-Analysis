6c085be556af5fde57d527eb3bf47492bf817b16
yourname <email>
2017-11-07 21:20:10 +0530

    added methods and docstrings

--- graphclass.py
+++ graphclass.py
@@ -1,21 +1,21 @@
 import networkx as ng
 
 class Graph:
-    
+    """Graph data structure : dict={node : [out],[in],attribute}"""
     def __init__(self,vertices=0,default_dict=None):
         self.vertices = vertices
         if default_dict==None:
             self.graph=dict()
             self.graph={}
     def add_vertex(self,vertex):
+    """Adds vertex to graph dict if not added"""
         if vertex not in self.graph.keys():
             self.graph[vertex]=[[None],[None]]
             self.vertices=self.vertices+1
             
     def add_edge(self,edgeset=[]):
-        """edgeset = [u,v,weight]"""
+        """edgeset = [u,v,weight], populates in and out accordingly"""
         self.edgeset=list(edgeset)
-  
         self.fanout=self.edgeset[1]
         self.fanin=self.edgeset[0]
         
@@ -34,16 +34,38 @@ class Graph:
     
         
     def num_vertices(self):
+        """Returns number of vertices"""
         return len(self.graph.keys())
     
     def get_vertices(self):
+        """returns a list of vertices"""
         return self.graph.keys()
     
     def get_in_degrees(self):
+        """Returns a vertex indexed list of in_degrees"""
         self.in_degrees = []
         for i in sorted(self.graph.keys()):
-            self.in_degrees.append(len(self.graph[i][1]))
+            if any(self.graph[i][1])==True:
+                self.in_degrees.append(len(self.graph[i][1]))
+            else:
+                self.in_degrees.append(0)
         return self.in_degrees
+    
+    def get_outneigbours(self,vertex):
+        """Returns a list containing out neigbours of a vertex"""
+        if vertex not in (self.graph.keys()):
+            return None
+        else:
+            return (self.graph[vertex][0])
+     
+    def get_inneigbours(self,vertex):
+        """Returns a list containing in neigbours of a vertex"""
+        if vertex not in (self.graph.keys()):
+            return None
+        else:
+            return (self.graph[vertex][1])
+    
+    
 g1=Graph(2)
 
 print g1.vertices
@@ -61,4 +83,5 @@ print g1.graph
 print g1.num_vertices()
 print g1.get_vertices()
 print g1.get_in_degrees()
+print g1.get_outneigbours(3)
 
